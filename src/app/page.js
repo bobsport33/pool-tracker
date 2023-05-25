50f3a7ff26d77d1612402dc8cc04b020e4694db8
bob <bkelly@impactnetworking.com>
2023-05-25 10:51:38 -0500

    adjust state updates

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,19 @@
+"use client";
+import { useState } from "react";
+import styled, { css } from "styled-components";
+
 import LeaderBoard from "../components/LeaderBoard";
 import Form from "../components/Form";
 import Hero from "../components/Hero";
 import GameSummary from "@/components/GameSummary";
+import DarkMode from "@/components/DarkMode";
+
+const MainCont = styled.main`
+    position: relative;
+    height: 100%;
+    width: 100%;
+    min-height: 100vh;
+`;
 
 export async function getData() {
     const response = await fetch(
@@ -15,16 +27,19 @@ export async function getData() {
 }
 
 export default async function Home() {
+    const [darkMode, setDarkMode] = useState(false);
     let data = await getData();
     let playerData = data.Players;
     let gameData = data.Games;
 
     return (
-        <main>
-            <Hero />
-            <LeaderBoard players={playerData} />
-            <Form players={playerData} results={gameData} />
-            <GameSummary results={gameData} />
-        </main>
+        <MainCont $darkMode={darkMode}>
+            <DarkMode>
+                <Hero />
+                <LeaderBoard players={playerData} />
+                <Form players={playerData} results={gameData} />
+                <GameSummary results={gameData} />
+            </DarkMode>
+        </MainCont>
     );
 }
---
+++ src/components/DarkMode.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import styled, { css } from "styled-components";
+
+const DarkModeCont = styled.div`
+    height: 100%;
+    width: 100%;
+    min-height: 100vh;
+    position: relative;
+
+    .dark-mode-btn {
+        position: absolute;
+        top: 10px;
+        right: 10px;
+    }
+
+    ${({ $darkMode }) => {
+        if ($darkMode) {
+            return css`
+                background-color: black;
+
+                h1,
+                h2,
+                h3,
+                h4,
+                h5,
+                h6,
+                p,
+                a {
+                    color: white;
+                }
+            `;
+        }
+    }}
+`;
+
+const DarkMode = ({ children }) => {
+    const [darkMode, setDarkMode] = useState(false);
+
+    const clickHandler = (e) => {
+        setDarkMode((current) => !current);
+
+        console.log(darkMode);
+    };
+
+    return (
+        <DarkModeCont $darkMode={darkMode}>
+            <button className="dark-mode-btn" onClick={clickHandler}>
+                Dark Mode
+            </button>
+            {children}
+        </DarkModeCont>
+    );
+};
+
+export default DarkMode;
