b64f85fc24729a0cbaec697012d49271b513fc06
bob <bkelly@impactnetworking.com>
2023-05-25 11:05:01 -0500

    adjustments

--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,8 @@
-"use client";
-import { useState } from "react";
-import styled, { css } from "styled-components";
-
 import LeaderBoard from "../components/LeaderBoard";
 import Form from "../components/Form";
 import Hero from "../components/Hero";
 import GameSummary from "@/components/GameSummary";
 import DarkMode from "@/components/DarkMode";
-
-const MainCont = styled.main`
-    position: relative;
-    height: 100%;
-    width: 100%;
-    min-height: 100vh;
-`;
 
 export async function getData() {
     const response = await fetch(
@@ -27,19 +16,18 @@ export async function getData() {
 }
 
 export default async function Home() {
-    const [darkMode, setDarkMode] = useState(false);
     let data = await getData();
     let playerData = data.Players;
     let gameData = data.Games;
 
     return (
-        <MainCont $darkMode={darkMode}>
+        <main>
             <DarkMode>
                 <Hero />
                 <LeaderBoard players={playerData} />
                 <Form players={playerData} results={gameData} />
                 <GameSummary results={gameData} />
             </DarkMode>
-        </MainCont>
+        </main>
     );
 }
