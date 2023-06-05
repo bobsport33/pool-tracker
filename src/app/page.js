165138f1121518c26fe1c7b37ec2a4d7e2b7cc3d
bob <bob62kelly@gmail.com>
2023-06-05 13:58:40 -0500

    ud data import

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,19 @@
 import GameResults from "@/components/GameResults";
-
-export async function getData() {
-    const response = await fetch(
-        "https://pool-records-default-rtdb.firebaseio.com/.json",
-        { cache: "no-store" }
-    );
-    if (!response.ok) {
-        throw new Error("failed to fetch data");
-    }
-    return response.json();
-}
+import axios from "axios";
+// export async function getData() {
+//     const response =
+//     if (!response.ok) {
+//         throw new Error("failed to fetch data");
+//     }
+//     return response.json();
+// }
 
 export default async function Home() {
-    let data = await getData();
+    let data = (
+        await axios.get(
+            "https://pool-records-default-rtdb.firebaseio.com/.json"
+        )
+    ).data;
     let playerData = data.Players;
     let gameData = data.Games;
 
