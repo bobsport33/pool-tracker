c7adf820fef4e6c72c45a7fbcc7ae8f390b7cc35
bob <bob62kelly@gmail.com>
2023-06-05 14:12:55 -0500

    ud

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,4 @@
 import GameResults from "@/components/GameResults";
-import axios from "axios";
-// export async function getData() {
-//     const response =
-//     if (!response.ok) {
-//         throw new Error("failed to fetch data");
-//     }
-//     return response.json();
-// }
 
 export default async function Home() {
     return (
