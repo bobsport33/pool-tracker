8e31f2e60bd1933c8bd669763ffd20eed0bfdaca
bob <bob62kelly@gmail.com>
2023-06-05 13:40:17 -0500

    ud

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import GameResults from "@/components/GameResults";
 export async function getData() {
     const response = await fetch(
         "https://pool-records-default-rtdb.firebaseio.com/.json",
-        { cache: "no-store" }
+        { cache: "no-cache" }
     );
     if (!response.ok) {
         throw new Error("failed to fetch data");
